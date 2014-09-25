b58fa8a2802695efab3bd5244e84df769990f6f4
Martijn van Exel <m@rtijn.org>
2014-09-25 12:40:08 -0600

    setuptools is better

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='overpass-wrapper',
-    packages=['overpass-wrapper'],
+    name='overpass',
+    packages=find_packages(),
     version='0.0.1',
     description='Python wrapper for the OpenStreetMap Overpass API',
     author='Martijn van Exel',
@@ -11,4 +11,5 @@ setup(
     download_url='https://github.com/mvexel/overpass-api-python-wrapper/tarball/0.0.1',
     keywords=['openstreetmap', 'overpass', 'wrapper'],
     classifiers=[],
+    install_requires=['requests>=2.3.0'],
 )
