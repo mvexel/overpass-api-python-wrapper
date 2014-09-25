75c368d724ca41fee0494a779f794327ab0073be
Martijn van Exel <m@rtijn.org>
2014-09-25 13:49:41 -0600

    adding tests

---
+++ test_api.py
@@ -0,0 +1,9 @@
+from overpass import Overpass
+
+
+class TestAPI:
+
+    def test_initialize_api(self):
+        api = Overpass.API()
+        assert isinstance(api, Overpass.API)
+        assert api.debug == False
