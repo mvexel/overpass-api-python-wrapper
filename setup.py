59f88ba657cdd5dae03f1aa7f22cea86f1f9a340
Martijn van Exel <m@rtijn.org>
2014-09-25 11:47:04 -0600

    structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='overpass-wrapper',
+    packages=['overpass-wrapper'],
+    version='0.0.1',
+    description='Python wrapper for the OpenStreetMap Overpass API',
+    author='Martijn van Exel',
+    author_email='m@rtijn.org',
+    url='https://github.com/mvexel/overpass-api-python-wrapper',
+    download_url='https://github.com/mvexel/overpass-api-python-wrapper/tarball/0.0.1',
+    keywords=['openstreetmap', 'overpass', 'wrapper'],
+    classifiers=[],
+)
