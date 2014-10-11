625c7499a920d56665923b48a2c49fc121dd96e9
Tim Bolender <git@timbolender.de>
2014-10-11 17:34:12 +0200

    Added auto-import for api.

---
+++ Overpass/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Thin wrapper around the OpenStreetMap Overpass API."""
+
+__title__ = 'overpass'
+__version__ = '0.0.1'
+__license__ = 'Apache 2.0'
+
+from .api import API
