52d922dbfa7646cae44d334c29522a14f64bae03
Martijn van Exel <m@rtijn.org>
2014-11-25 14:23:57 -0700

    pep8 compliance, implementing raw_query

--- test_api.py
+++ test_api.py
@@ -12,7 +12,7 @@ class TestAPI(object):
         api = overpass.API()
         #osm_elements = api.Get(overpass.MapQuery(37.86517,-122.31851,37.86687,-122.31635))
         #print 'DEB osm_elements:', geojson.dumps(osm_elements,sort_keys=True,indent=2)
-        osm_geo = api.Get(overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635), asGeoJSON=True)
+        osm_geo = api.get(overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635), asGeoJSON=True)
         #with open('test.geojson','w') as f:
         #    geojson.dump(osm_geo,f,indent=2,sort_keys=True)
         assert len(osm_geo['features']) > 1
