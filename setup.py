9414737768506f16262ef065f5988d9629926f03
Wille Marcel <wille@wille.blog.br>
2015-02-08 21:44:43 -0200

    improve setup.py and .gitignore and create tests directory

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from setuptools import setup
 setup(
     name='overpass',
     packages=['overpass'],
-    version='0.1.0',
+    version='0.1.1',
     description='Python wrapper for the OpenStreetMap Overpass API',
     author='Martijn van Exel',
     author_email='m@rtijn.org',
     url='https://github.com/mvexel/overpass-api-python-wrapper',
+    license='Apache',
     download_url='https://github.com/mvexel/overpass-api-python-wrapper/tarball/0.0.1',
     keywords=['openstreetmap', 'overpass', 'wrapper'],
     classifiers=[],
     install_requires=['requests>=2.3.0', 'geojson>=1.0.9'],
+    extras_require={
+        'test': ['pytest'],
+    },
 )
