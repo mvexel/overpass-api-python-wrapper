948df9222f8256cdea1a34efc644d12dcb2c447c
Wille Marcel <wille@wille.blog.br>
2015-10-12 16:56:04 -0300

    avoid MultipleRequestsError by using two differents end points on tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,9 +10,9 @@ def test_cli():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(cli, [
-            '--timeout', 30,
+            '--timeout', 40,
             '--endpoint', 'http://overpass-api.de/api/interpreter',
-            'node(area:3602758138)[amenity=cafe]', 'out.geojson'
+            'node(area:3600362504)[amenity=cafe]', 'out.geojson'
             ])
         assert result.exit_code == 0
         assert exists('out.geojson')
@@ -22,10 +22,10 @@ def test_cli_xml():
     runner = CliRunner()
     with runner.isolated_filesystem():
         result = runner.invoke(cli, [
-            '--timeout', 30,
-            '--endpoint', 'http://overpass-api.de/api/interpreter',
+            '--timeout', 40,
+            '--endpoint', 'http://overpass.osm.rambler.ru/cgi/interpreter',
             '--format', 'osm',
-            'node(area:3602758138)[amenity=cafe]', 'out.osm'
+            'node(area:3600362504)[amenity=cafe]', 'out.osm'
             ])
         assert result.exit_code == 0
         assert exists('out.osm')
