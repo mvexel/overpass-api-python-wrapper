ab3c67dc4c7ab83acfd7fd2e2909fa2456a622cb
Martijn van Exel <m@rtijn.org>
2015-12-14 14:25:07 -0700

    change format to responseformat (format is a reserved word in Python)

--- overpass/cli.py
+++ overpass/cli.py
@@ -10,20 +10,20 @@ import overpass
 @click.option('--timeout', default=25, help='Timeout in seconds.')
 @click.option('--endpoint', default='http://overpass-api.de/api/interpreter',
     help='URL of your prefered API.')
-@click.option('--format', default='geojson', help="""Format to save the data.
+@click.option('--responseformat', default='geojson', help="""Format to save the data.
     Options are 'geojson' and 'osm'. Default format is geojson.""")
 @click.argument('query', type=str)
 @click.argument('output_file', type=click.Path())
-def cli(timeout, endpoint, format, query, output_file):
+def cli(timeout, endpoint, responseformat, query, output_file):
     """Run query and save the result in output_file"""
 
     api = overpass.API(timeout=timeout, endpoint=endpoint)
 
-    if format not in api.SUPPORTED_FORMATS:
+    if responseformat not in api.SUPPORTED_FORMATS:
         print("format {} not supported. Supported formats: {}".format(
-            format,
+            responseformat,
             api.SUPPORTED_FORMATS.join(", ")))
-    result = api.Get(query, responseformat=format)
+    result = api.Get(query, responseformat=responseformat)
     with open(output_file, 'w') as f:
         if responseformat=="geojson":
             geojson.dump(result, f, indent=2, sort_keys=True)
