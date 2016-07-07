e7c5603bb9f3e4edb7e7cdeef36c9429816ada44
nebulon42 <nebulon42@yandex.com>
2016-07-07 21:26:52 +0200

    enable csv response format

--- overpass/api.py
+++ overpass/api.py
@@ -44,7 +44,7 @@ class API(object):
         # Get the response from Overpass
         raw_response = self._GetFromOverpass(full_query)
 
-        if responseformat == "xml":
+        if responseformat == "xml" or responseformat.startswith("csv"):
             return raw_response
             
         response = json.loads(raw_response)
