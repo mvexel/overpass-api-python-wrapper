198aeb212c2d0ae9d58ca8333bc66395cd10b9f1
Martijn van Exel <m@rtijn.org>
2018-04-06 11:32:20 -0600

    fixing var name in test script and remove function call

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,13 +10,10 @@ def test_initialize_api():
 def test_geojson():
     api = overpass.API(debug=True)
 
-    MapQuery = overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635)
-    print MapQuery
-    osm_geo = api.get(MapQuery)
+    map_query = overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635)
+    osm_geo = api.get(map_query)
     print osm_geo
     assert len(osm_geo['features']) > 1
 
     osm_geo = api.get('node(area:3602758138)[amenity=cafe]')
     assert len(osm_geo['features']) > 1
-
-test_geojson()
