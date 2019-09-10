f6ba57a9c09a124be6dc9e072c6fc484244e92c7
Lukas Müller <l.muller@student.tudelft.nl>
2019-09-10 23:40:39 +0200

    Added extended geojson test based on example data

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,9 @@
 # See LICENSE.txt for the full license text.
 
 import overpass
+import geojson
+import pickle
+import os
 
 
 def test_initialize_api():
@@ -21,3 +24,22 @@ def test_geojson():
 
     osm_geo = api.get("node(area:3602758138)[amenity=cafe]")
     assert len(osm_geo["features"]) > 1
+
+
+def test_geojson_extended():
+
+    class API(overpass.API):
+        def _get_from_overpass(self, query):
+            return pickle.load(open(os.path.join(os.path.dirname(__file__), "example.response"), "rb"))
+
+    # api = overpass.API()
+    # osm_geo = api.get("rel(6518385);out body geom;way(10322303);out body geom;node(4927326183);", verbosity='body geom')
+    # pickle.dump(api._get_from_overpass("[out:json];rel(6518385);out body geom;way(10322303);out body geom;node(4927326183);out body geom;"),
+    #             open(os.path.join(os.path.dirname(__file__), "example.response"), "wb"),
+    #             protocol=2)
+    # geojson.dump(osm_geo, open(os.path.join(os.path.dirname(__file__), "example.json"), "w"))
+
+    api = API()
+    osm_geo = api.get("rel(6518385);out body geom;way(10322303);out body geom;node(4927326183);", verbosity='body geom')
+    ref_geo = geojson.load(open(os.path.join(os.path.dirname(__file__), "example.json"), "r"))
+    assert osm_geo==ref_geo
