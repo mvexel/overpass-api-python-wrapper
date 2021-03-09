b39ce8be7d7486d2b8af7a220d07e97269d0e656
Evan Derickson <evan.derickson@kaart.com>
2021-03-09 10:35:57 -0800

    Parametrize geojson test

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,15 +18,22 @@ def test_initialize_api():
     assert api.debug is False
 
 
-def test_geojson():
+@pytest.mark.parametrize(
+    "query,length",
+    [
+        (overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635), 1),
+        ("node(area:3602758138)[amenity=cafe]", 1)
+    ]
+)
+def test_geojson(
+    query,
+    length
+):
     api = overpass.API(debug=True)
 
-    map_query = overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635)
-    osm_geo = api.get(map_query)
-    assert len(osm_geo["features"]) > 1
+    osm_geo = api.get(query)
+    assert len(osm_geo["features"]) > length
 
-    osm_geo = api.get("node(area:3602758138)[amenity=cafe]")
-    assert len(osm_geo["features"]) > 1
 
 
 def test_geojson_extended():
