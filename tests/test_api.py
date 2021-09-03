b40baa9c60f8f70e4a28b6f395fa4e62b97110f6
Vitali Balandin <vitali.balandin@srini.ee>
2021-09-03 17:12:44 +0300

    refactor, parametrize test_geojson

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,12 @@
 import overpass
 import geojson
 import pickle
+import pytest
 import os
+
+
+PARAM_GEOJSON = (overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635),
+                 'node(area:3602758138)[amenity=cafe]')
 
 
 def test_initialize_api():
@@ -15,15 +20,11 @@ def test_initialize_api():
     assert api.debug is False
 
 
-def test_geojson():
+@pytest.mark.parametrize('query', PARAM_GEOJSON)
+def test_geojson(query):
     api = overpass.API(debug=True)
-
-    map_query = overpass.MapQuery(37.86517, -122.31851, 37.86687, -122.31635)
-    osm_geo = api.get(map_query)
-    assert len(osm_geo["features"]) > 1
-
-    osm_geo = api.get("node(area:3602758138)[amenity=cafe]")
-    assert len(osm_geo["features"]) > 1
+    osm_geo = api.get(query)
+    assert osm_geo["features"]
 
 
 def test_geojson_extended():
@@ -52,4 +53,4 @@ def test_geojson_extended():
     api = API()
     osm_geo = api.get("rel(6518385);out body geom;way(10322303);out body geom;node(4927326183);", verbosity='body geom')
     ref_geo = geojson.load(open(os.path.join(os.path.dirname(__file__), "example.json"), "r"))
-    assert osm_geo==ref_geo
+    assert osm_geo == ref_geo
