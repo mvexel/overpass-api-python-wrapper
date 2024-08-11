39a4a12d65416b327208c1dbb9b1811ac03b0222
Martijn van Exel <m@rtijn.org>
2024-08-11 17:48:27 -0600

    chore:moderizing code

--- overpass/utils.py
+++
@@ -1,9 +0,0 @@
-class Utils(object):
-
-    @staticmethod
-    def to_overpass_id(osmid, area=False):
-        area_base = 2400000000
-        relation_base = 3600000000
-        if area:
-            return int(osmid) + area_base
-        return int(osmid) + relation_base
